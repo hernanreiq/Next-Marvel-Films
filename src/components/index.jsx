b57c545bb90ffe597652adf480059fa941a57dc6
Reiq.Laptop <reiqvideos@gmail.com>
2021-11-15 02:54:36 +0100

    Archivos iniciales listos

---
+++ src/components/index.jsx
@@ -0,0 +1,13 @@
+import { Component, Fragment } from "react";
+
+class Index extends Component {
+    render() {
+        return (
+            <Fragment>
+                
+            </Fragment>
+        )
+    }
+}
+
+export default Index;
